46e5c28c2b2595e01a0818a878f4d7ee802bba57
notGriffin <krima724@gmail.com>
2025-11-04 09:02:56 -0600

    summary

--- Employee.py
+++ Employee.py
@@ -1,10 +1,31 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+# Program #4 EmployeeDatabase, Griffin Corneia, 11/3/25:
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+class Employee:
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+    def __init__(self, name, id_number, department, job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
+
+    def display_info(self):
+        print(self.name)
+        print(self.id_number)
+        print(self.department)
+        print(self.job_title)
+        print("")
+
+def main():
+    print("Employee Information")
+    print("")
+
+    employee1 = Employee("Susan Meyers", 47899, "Accounting", "Vice President")
+    employee2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+    employee3 = Employee("Joy Rogers", 81774, "Manufacturing", "Engineer")
+
+    employee1.display_info()
+    employee2.display_info()
+    employee3.display_info()
+
+if __name__ == "__main__":
+    main()
